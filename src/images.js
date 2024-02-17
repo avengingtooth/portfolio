bc559a250203bfedc28448af1abe5a6129f8daac
avengingtooth <esellin@gmail.com>
2024-02-17 17:12:29 +0100

    mole mash

--- src/images.js
+++ src/images.js
@@ -14,11 +14,16 @@ import MandalaQrCode from './images/Mandala/qrcode.png'
 import MandalaCode from './images/Mandala/code.png'
 import MandalaScreen from './images/Mandala/screen.png'
 
+import MoleMashQrCode from './images/MoleMash/qrcode.png'
+import MoleMashCode from './images/MoleMash/code.png'
+import MoleMashScreen from './images/MoleMash/screen.png'
+
 function qrCode(index){
     if(index === 0) return IHaveADreamQrCode;
     else if(index === 1) return BirdQrCode;
     else if(index === 2) return PaintPotQrCode;
     else if(index === 3) return MandalaQrCode;
+    else if(index === 4) return MoleMashQrCode;
     else return ""
 }
 
@@ -27,6 +32,7 @@ function code(index){
     else if(index === 1) return BirdCode;
     else if(index === 2) return PaintPotCode;
     else if(index === 3) return MandalaCode;
+    else if(index === 4) return MoleMashCode;
     else return ""
 }
 
@@ -35,6 +41,7 @@ function screen(index){
     else if(index === 1) return BirdScreen;
     else if(index === 2) return PaintPotScreen;
     else if(index === 3) return MandalaScreen;
+    else if(index === 4) return MoleMashScreen;
     else return ""
 }
 
