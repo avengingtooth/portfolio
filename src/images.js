ec2bb2eb3dc998818e53732d2f15f1d04639ada4
avengingtooth <esellin@gmail.com>
2024-02-17 11:21:51 +0100

    paint pot

--- src/images.js
+++ src/images.js
@@ -1,25 +1,33 @@
 import IHaveADreamQrCode from "./images/IHaveADream/qrcode.png"
 import IHaveADreamCode from "./images/IHaveADream/code.png"
 import IHaveADreamScreen from "./images/IHaveADream/screen.png"
+
 import BirdQrCode from "./images/Bird/qrcode.png"
 import BirdCode from "./images/Bird/code.png"
 import BirdScreen from "./images/Bird/screen.png"
 
+import PaintPotQrCode from './images/PaintPot/qrcode.png'
+import PaintPotCode from './images/PaintPot/code.png'
+import PaintPotScreen from './images/PaintPot/screen.png'
+
 function qrCode(index){
     if(index === 0) return IHaveADreamQrCode;
     else if(index === 1) return BirdQrCode;
+    else if(index === 2) return PaintPotQrCode;
     else return ""
 }
 
 function code(index){
     if(index === 0) return IHaveADreamCode;
     else if(index === 1) return BirdCode;
+    else if(index === 2) return PaintPotCode;
     else return ""
 }
 
 function screen(index){
     if(index === 0) return IHaveADreamScreen;
     else if(index === 1) return BirdScreen;
+    else if(index === 2) return PaintPotScreen;
     else return ""
 }
 
